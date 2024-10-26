64fb685b0dcdbcaa5c6bd62b5cee9ad6e4b868f4
davidtavartk <d.tavartkiladze5@gmail.com>
2024-10-26 22:39:15 +0400

    bonus task

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+          sans: ['Karla', 'sans-serif'],
+          dmSerif: ['"DM Serif Display"', 'serif'],
+      },
+  },
+  },
+  plugins: [],
+}
+
